404de6926ca3fadeac5a6430d60dd261d14c5c70
Alex Berg <a.vandenberg-2@tudelft.nl>
2024-06-05 23:53:21 +0200

    renaming

---
+++ runtime/source/data_loading.R
@@ -0,0 +1,210 @@
+################ PATH DEFINITIONS ################ 
+dataFolder <- file.path(".", "data")
+participants <- list.dirs(path = dataFolder, full.names = FALSE, recursive = FALSE)
+
+# Path definitions
+questionnaireInfoFolder <- file.path(".", "questionnaires")
+trackerPath <- file.path(file.path(dataFolder, participants[1]), "trackers")
+#trackers <- list.files(path = trackerPath, full.names = FALSE)
+filenameDict <- list(
+  "camera" = "camera_movement_T",
+  "hip" = "hiptracker_movement_T",
+  "leftfoot" = "leftfoottracker_movement_T",
+  "lefthand" = "lefthandtracker_movement_T",
+  "rightfoot" = "rightfoottracker_movement_T",
+  "righthand" = "righthandtracker_movement_T",
+  "steptargets" = "steptargetsmanager_targetsteps_T",
+  "treadmillleft" = "treadmillleft_movement_T",
+  "treadmillright" = "treadmillright_movement_T",
+  "treadmillback" = "treadmillrightback_movement_T",
+  "eye" = "eyetracking_EyeGaze_T",
+  "leftdisturbance" = "leftfoot_disturbance_noise_T",
+  "rightdisturbance" = "rightfoot_disturbance_noise_T"
+)
+
+trackers <- names(filenameDict)
+
+################ Data retrieval / helper methods ################ 
+
+# Data retrieval functions
+get_p_dir <- function(pnum) {
+  return(file.path(dataFolder, pnum))
+}
+
+get_p_resultsFile <- function(pnum) {
+  return(file.path(get_p_dir(pnum), "trial_results.csv"))
+}
+
+get_p_results <- function(pnum, settingName, trialNumber) {
+  # get the path to the settings file for the participant
+  resultsFile <- get_p_resultsFile(pnum)
+  results <- read.csv(resultsFile)
+  
+  # retrieve the value of the specific detail
+  resultValue <- results[[settingName]][trialNumber]
+  
+  return(resultValue)
+}
+
+get_move_speed <- function(pnum) { # return move speed in m/s
+  trialNum = 1 # should be the same for all trials
+  return(get_p_results(pnum,"move_speed",trialNum) / 3.6)
+}
+
+get_p_detail <- function(pnum, detail) {
+  # get the path to the details file for the participant
+  detailsFile <- file.path(get_p_dir(pnum), "session_info/participant_details.csv")
+  
+  # read the csv file into a data frame
+  details <- read.csv(detailsFile)
+  
+  # retrieve the value of the specific detail
+  detailValue <- details[[detail]][1]
+  
+  return(detailValue)
+}
+
+# here we make a big table to show us the demographics summary of our participant pool
+calculate_participant_details <- function(participants) {
+  details <- data.frame(participant = participants)
+  
+  # Retrieve details for each participant
+  details$age               <- sapply(participants, get_p_detail, detail = "age")
+  details$gender            <- sapply(participants, get_p_detail, detail = "gender")
+  details$startedWithNoise  <- sapply(participants, started_with_noise)
+  details$education         <- sapply(participants, get_p_detail, detail = "education")
+  details$vr_experience     <- sapply(participants, get_p_detail, detail = "vr_experience")
+  details$motion            <- sapply(participants, get_p_detail, detail = "motion")
+  details$noticed           <- sapply(participants, get_p_detail, detail = "noticed")
+  
+  # Calculate statistics for age
+  age_mean <- mean(details$age)
+  age_sd <- sd(details$age)
+  age_median <- median(details$age)
+  age_min <- min(details$age)
+  age_max <- max(details$age)
+  age_iqr <- IQR(details$age)
+  
+  # Format the age results
+  age_mean_sd <- sprintf("%.2f, SD: %.2f", age_mean, age_sd)
+  age_median_min_max_iqr <- sprintf("%d [%.0f, %.0f], IQR=%.2f", age_median, age_min, age_max, age_iqr)
+  
+  # Count the occurrences of each unique value in other columns
+  gender_counts <- table(details$gender)
+  started_counts <- table(details$startedWithNoise)
+  education_counts <- table(details$education)
+  vr_experience_counts <- table(details$vr_experience)
+  motion_counts <- table(details$motion)
+  noticed_counts <- table(details$noticed)
+  
+  # Create a result data frame
+  result <- data.frame(
+    Detail = c(
+      "Age (Mean, SD)", "Age (Median [Min, Max], IQR)", 
+      rep("Gender", length(gender_counts)), 
+      rep("Started With Noise", length(started_counts)),
+      rep("Education", length(education_counts)), 
+      rep("VR Experience", length(vr_experience_counts)), 
+      rep("Motion", length(motion_counts)), 
+      rep("Noticed", length(noticed_counts))
+    ),
+    Value = c(
+      age_mean_sd, age_median_min_max_iqr, 
+      as.vector(gender_counts), 
+      as.vector(started_counts), 
+      as.vector(education_counts), 
+      as.vector(vr_experience_counts), 
+      as.vector(motion_counts), 
+      as.vector(noticed_counts)
+    )
+  )
+  
+  # Add the names of the counted categories to the details
+  result$Category <- c(
+    "", "", 
+    names(gender_counts), 
+    names(started_counts), 
+    names(education_counts), 
+    names(vr_experience_counts), 
+    names(motion_counts), 
+    names(noticed_counts)
+  )
+  
+  return(result)
+}
+
+# If true, the participant started with noise VFD enabled, otherwise without it enabled
+started_with_noise <- function(pnum) {
+  secondTrialHasNoise <- get_p_results(pnum,"noise_enabled",2) == "True"
+  return(secondTrialHasNoise)
+}
+
+# did participant notice the VFD?
+noticed_vfd <- function(participant) {
+  return(get_p_detail(participant, "noticed")=="True")
+}
+
+# get any type of data
+get_t_data <- function(pnum, trackerType, trialNum) {
+  # Validate trackerType
+  if (!trackerType %in% names(filenameDict)) {
+    stop("Invalid tracker type specified.")
+  }
+  
+  filename <- paste0(filenameDict[[trackerType]], sprintf("%03d", trialNum), ".csv")
+  filePath <- file.path(get_p_dir(pnum), "trackers", filename)
+  
+  # Use tryCatch for more robust error handling
+  tryCatch(
+    {
+      data <- read.csv(filePath)
+    },
+    error = function(e) {
+      message("Failed to read the file: ", e$message)
+      return(NULL) # Or handle the error as appropriate for your context
+    }
+  )
+  
+  return(data)
+}
+
+# For changing up the selection inputs - bit costly, but not really needed to optimize this
+getOptions <- function(tracker){
+  exampleData <- get_t_data(participants[1],tracker,1)
+  numericTypes <- sapply(exampleData, is.numeric)
+  numeric_cols <- names(exampleData[numericTypes]) # names(numericDataTypes[numericDataTypes | logicalDataTypes])
+  return(numeric_cols)
+}
+
+get_q_file <- function(pnum, qType) { # qType = IMI / SSQ / VEQ
+  return(file.path(get_p_dir(pnum), "Questionnaires", paste0("questionnaireID_", qType, "_ALL_answers.csv")))
+}
+
+################ Questionnaires ################
+
+get_q_data <- function(pnum, qType) {
+  # Get the path to the questionnaire file for the participant
+  questionnaireFile <- get_q_file(pnum, qType)
+  
+  # Read the CSV file into a data frame
+  questionnaire <- read.csv(questionnaireFile)
+  
+  # Extract the QuestionID and the two answer columns
+  result <- questionnaire[, c("QuestionID", "Answer_Participant__condition_Base", "Answer_Participant__condition_Noise")]
+  
+  return(result)
+}
+
+get_question_info <- function(qType) { # qType = IMI / SSQ / VEQ
+  qInfopath <- file.path(questionnaireInfoFolder, paste0(qType, ".csv"))
+  # Read the CSV file into a data frame
+  questionnaire <- read.csv(qInfopath)
+  return(questionnaire)
+}
+
+get_question_weights <- function(qType) { # qType = IMI / SSQ / VEQ
+  qpath <- file.path(questionnaireInfoFolder, paste0(qType, "_weights.csv"))
+  # Read the CSV file into a data frame
+  questionnaire <- read.csv(qpath)
+  return(questionnaire)
+}
