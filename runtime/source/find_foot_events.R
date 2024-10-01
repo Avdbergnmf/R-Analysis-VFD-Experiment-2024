58493996996f22a330c5cdbaaacd1175ad6839a0
Alex Berg <a.vandenberg-2@tudelft.nl>
2024-10-01 22:05:50 +0200

    Filtering settings used in final data processing.

--- runtime/source/find_foot_events.R
+++ runtime/source/find_foot_events.R
@@ -1,4 +1,4 @@
-################ Data Manipulation ################ 
+################ Data Manipulation ################
 
 # Find the final positions by: foot position + noise values & add it to the DF
 calc_final_pos <- function(footData, noiseData) {
@@ -7,7 +7,7 @@ calc_final_pos <- function(footData, noiseData) {
   noiseData <- noiseData %>%
     select(time, offset_x, offset_y, offset_z) %>%
     arrange(time)
-  
+
   # Join footData with noiseData by time
   footData <- footData %>%
     left_join(noiseData, by = "time") %>%
@@ -19,154 +19,184 @@ calc_final_pos <- function(footData, noiseData) {
       final_pos_z = pos_z + offset_z
     ) %>%
     ungroup()
-  
+
   return(footData)
 }
 
+
+
 detect_foot_events_coordinates <- function(footData, hipData) {
   ####### FILTER
-  # Apply a moving average filter to the relative foot position
-  numeric_columns <- sapply(hipData, is.numeric)  # Identify numeric columns --> NOTE: actual_pos_z not filtered, but not needed here.
-  
-  # Apply the filter to all numeric columns
-  poly_order <- 4  # Order of the polynomial (savgoy=3,butter=4)
-  frame_size <- 5  # (for savgoy Must be odd)
-  footData_filtered <- footData
-  hipData_filtered  <- hipData
-  footData_filtered[numeric_columns] <- lapply(footData[numeric_columns], function(column) { apply_padding_and_filter(column, poly_order, frame_size, 90) } ) 
-  hipData_filtered[numeric_columns] <- lapply(hipData[numeric_columns], function(column) { apply_padding_and_filter(column, poly_order, frame_size, 90) } )
-  
-  # Get subcomponents
-  frontalFootPos_filtered <- footData_filtered$pos_z
-  frontalHipPos_filtered <- hipData_filtered$pos_z
-  #footHeight <- footData$pos_y ##### Could also do a height check, but looks good without it
-  relFootPos_filtered <- frontalFootPos_filtered - frontalHipPos_filtered
-  
+  relFootPos <- footData$pos_z - hipData$pos_z
+  relFootPos_filtered <- apply_padding_and_filter(relFootPos, 4, 90, 5)
+
   # Detect local extremes of relative foot pos - Based on https://c-motion.com/v3dwiki/index.php/Tutorial:_Gait_Events#Method_1._Coordinate_Based_Algorithm
   local_maxima <- which(diff(sign(diff(relFootPos_filtered))) == -2) + 1
   local_minima <- which(diff(sign(diff(relFootPos_filtered))) == 2) + 1
-  if (local_minima[1] > local_maxima[1]) {
+  if (local_minima[1] > local_maxima[1]) { # Always start with a minimum (toe-off)
     local_maxima <- local_maxima[-1]
   }
+
+  # Check for minimum time difference between consecutive maxima and minima  (erronous detections)
+  time_vector <- footData$time
+  pos_vector <- relFootPos_filtered
+  i <- 1 # Initialize indice
+  min_time_diff <- 0.05
+  min_pos_diff <- 0.0
+  N_removed_time <- 0
+  N_removed_pos <- 0
+  while (i < length(local_maxima) && i < length(local_minima)) {
+    # Check time difference
+    time_diff_max_to_min <- abs(time_vector[local_maxima[i]] - time_vector[local_minima[i + 1]])
+    time_diff_min_to_max <- abs(time_vector[local_minima[i]] - time_vector[local_maxima[i]])
+    # Check distance difference
+    pos_diff_max_to_min <- abs(pos_vector[local_maxima[i]] - pos_vector[local_minima[i + 1]])
+    pos_diff_min_to_max <- abs(pos_vector[local_minima[i]] - pos_vector[local_maxima[i]])
+
+    if (time_diff_max_to_min < min_time_diff) {
+      local_minima <- local_minima[-(i + 1)]
+      local_maxima <- local_maxima[-i]
+      N_removed_time <- N_removed_time + 1
+    } else if (time_diff_min_to_max < min_time_diff) {
+      local_maxima <- local_maxima[-i]
+      local_minima <- local_minima[-i]
+      N_removed_time <- N_removed_time + 1
+    } else if (pos_diff_max_to_min < min_pos_diff) { # we need to nest these, to prevent removing twice in 1 loop
+      local_minima <- local_minima[-(i + 1)]
+      local_maxima <- local_maxima[-i]
+      N_removed_pos <- N_removed_pos + 1
+    } else if (pos_diff_min_to_max < min_pos_diff) {
+      local_maxima <- local_maxima[-i]
+      local_minima <- local_minima[-i]
+      N_removed_pos <- N_removed_pos + 1
+    } else {
+      # Move to the next pair if both time and pos differences are sufficient
+      i <- i + 1
+    }
+  }
   
+  #local_maxima <- local_maxima[relFootPos_filtered[local_maxima] > 0]
+  #local_minima <- local_minima[relFootPos_filtered[local_minima] < 0]
+  
+  # alternation checking
   N_removed_min <- 0
   N_removed_max <- 0
-  # alternation checking
   for (i in 1:(length(local_minima))) {
     if (i <= length(local_minima) && i <= length(local_maxima)) {
       while (local_maxima[i] < local_minima[i] && i <= length(local_minima) && i <= length(local_maxima)) {
-        local_maxima <- local_maxima[-i] # remove the maxima, it is wrong
+        local_maxima <- local_maxima[-i] # remove the maximum, it is wrong
         N_removed_min <- N_removed_min + 1
       }
     }
-    
-    if (i+1 <= length(local_minima)  && i <= length(local_maxima)) {
-      while (local_maxima[i] > local_minima[i+1] && i+1 <= length(local_minima)  && i <= length(local_maxima)) {
-        local_minima <- local_minima[-(i+1)]
+
+    if (i + 1 <= length(local_minima) && i <= length(local_maxima)) {
+      while (local_maxima[i] > local_minima[i + 1] && i + 1 <= length(local_minima) && i <= length(local_maxima)) {
+        local_minima <- local_minima[-(i + 1)] # remove the minimum, it is wrong
         N_removed_max <- N_removed_max + 1
       }
     }
   }
-  
+
   # Make sure lengths match
   lMax <- length(local_maxima)
   lMin <- length(local_minima)
-  if (lMax != lMin){
-    if (lMax > lMin){print("Something REALLY WRONG... check detect_foot_events_coordinates()")}
-    
+  if (lMax != lMin) {
+    if (lMax > lMin) {
+      print("Something REALLY WRONG... check detect_foot_events_coordinates()")
+    }
+
     trimLength <- min(lMax, lMin)
     local_maxima <- local_maxima[1:trimLength]
     local_minima <- local_minima[1:trimLength]
   }
-  
+
   # Some logging
-  if (N_removed_max + N_removed_min > 0) {
-    print(paste("removed",N_removed_max,"maxima, and",N_removed_min,"minima."))
+  print(paste("removed", N_removed_time, "max+min due to time constraint, and", N_removed_pos, "max+min due to pos difference constraint."))
+  print(paste("removed", N_removed_max, "maxima, and", N_removed_min, "minima due to wrong alternation."))
+  if (length(local_maxima) != length(local_minima)) {
+    print(paste("Length maxima:", length(local_maxima), "Length minima:", length(local_minima)))
   }
-  if (length(local_maxima) !=length(local_minima)) {  
-    print(paste("Length maxima:",length(local_maxima),"Length minima:",length(local_minima)))
-  }
-  
+
   # Extract positions and times - use UNFILTERED footData
   heelStrikes <- data.frame(footData[local_maxima, ])
   toeOffs <- data.frame(footData[local_minima, ])
-  print(paste("--- ---totalsteps: ",length(heelStrikes$time)))
-  return(list(heelStrikes=heelStrikes,toeOffs=toeOffs))
+  print(paste("--- ---totalsteps: ", length(heelStrikes$time)))
+  return(list(heelStrikes = heelStrikes, toeOffs = toeOffs))
 }
 
 find_foot_events <- function(participant, trialNum) {
   preprocessedData <- preprocess_data(participant, trialNum)
-  
+
   leftFoot <- preprocessedData$leftFoot
   rightFoot <- preprocessedData$rightFoot
   hip <- preprocessedData$hip
   targetData <- preprocessedData$targetData
-  
+
   # Detect toe-off and heelstrikes
   footEventsLeft <- detect_foot_events_coordinates(leftFoot, hip)
   footEventsRight <- detect_foot_events_coordinates(rightFoot, hip)
-  
+
   # Add a 'foot' column to each event dataframe
-  footEventsLeft$heelStrikes$foot <- 'Left'
-  footEventsLeft$toeOffs$foot <- 'Left'
-  footEventsRight$heelStrikes$foot <- 'Right'
-  footEventsRight$toeOffs$foot <- 'Right'
-  
+  footEventsLeft$heelStrikes$foot <- "Left"
+  footEventsLeft$toeOffs$foot <- "Left"
+  footEventsRight$heelStrikes$foot <- "Right"
+  footEventsRight$toeOffs$foot <- "Right"
+
   # Combine heel strikes and foot lifts from both feet
   combinedHeelStrikes <- rbind(footEventsLeft$heelStrikes, footEventsRight$heelStrikes)
   combinedToeOffs <- rbind(footEventsLeft$toeOffs, footEventsRight$toeOffs)
-  
+
   # Order the events by time
   combinedHeelStrikes <- combinedHeelStrikes[order(combinedHeelStrikes$time), ]
   combinedToeOffs <- combinedToeOffs[order(combinedToeOffs$time), ]
-  
+
   ensure_alternation <- function(data1, data2) {
     incorrect_seq <- which(diff(as.numeric(data1$foot == "Left")) == 0)
     
     if (length(incorrect_seq > 0)) {
-      #print(data1[c(incorrect_seq,incorrect_seq+1),])
-      data1 <- data1[-(incorrect_seq + 1),] # remove the second (later) value
-      data2 <- data2[-(incorrect_seq + 1),] # remove the second (later) value
-      print(paste("--- ---removed",length(incorrect_seq),"steps total (wrong alternation). Removing at place:",incorrect_seq))
-      #print("----------------------")
+      # print(data1[c(incorrect_seq,incorrect_seq+1),])
+      data1 <- data1[-(incorrect_seq + 1), ] # remove the second (later) value
+      data2 <- data2[-(incorrect_seq + 1), ] # remove the second (later) value
+      print(paste("--- ---removed", length(incorrect_seq), "steps total (wrong alternation). Removing at place:", incorrect_seq))
+      # print("----------------------")
     }
     return(list(data1 = data1, data2 = data2))
   }
-  
+
   # Apply alternation check on heelStrikes and remove index columns
   results <- ensure_alternation(combinedHeelStrikes, combinedToeOffs)
   combinedHeelStrikes <- results$data1
-  combinedToeOffs     <- results$data2
-  
+  combinedToeOffs <- results$data2
+
   # Label step numbers. Assuming each heel strike represents a new step
   combinedHeelStrikes$step <- seq_len(nrow(combinedHeelStrikes))
   combinedToeOffs$step <- seq_len(nrow(combinedToeOffs))
-  
+
   # Check if we stepped onto a target
   targetData <- targetData[order(targetData$time), ] # make sure we order by time
   combinedHeelStrikes$target <- FALSE
   for (i in 2:nrow(combinedHeelStrikes)) {
-    prevStepTime <- combinedHeelStrikes$time[i-1]
+    prevStepTime <- combinedHeelStrikes$time[i - 1]
     stepTime <- combinedHeelStrikes$time[i]
-    
+
     # Find targets between the current heel strike and the next heel strike (or end of recording)
     targetsInTime <- targetData[targetData$time > prevStepTime & targetData$time <= stepTime, ]
-    
+
     # Update the target_stepped_on flag if any targets were found
     if (nrow(targetsInTime) > 0) {
       combinedHeelStrikes$target[i] <- TRUE
     }
   }
-  
+
   return(list(heelStrikes = combinedHeelStrikes, toeOffs = combinedToeOffs))
 }
 
 calculate_gait_parameters <- function(participant, trialNum) {
   gaitData <- find_foot_events(participant, trialNum)
-  
+
   heelStrikesData <- gaitData$heelStrikes # should already be sorted based on time
   toeOffsData <- gaitData$toeOffs
-  
+
   relHeelStrikesData <- gaitData$heelStrikes
   # Apply diff and padding to each numeric column
   relHeelStrikesData[] <- lapply(relHeelStrikesData, function(column) {
@@ -174,70 +204,78 @@ calculate_gait_parameters <- function(participant, trialNum) {
       # Calculate differences and pad with a leading zero
       c(0, diff(column))
     } else {
-      column  # Return non-numeric columns unchanged
+      column # Return non-numeric columns unchanged
     }
   })
-  
+
   # time-based
   stepTimes <- relHeelStrikesData$time # Calculate step times  >>> NOTE: The first heelstrike is only used as a starting point to the second
   swingTimes <- heelStrikesData$time - toeOffsData$time # Calculate swing times <<< L = N   (not N-1)
   stanceTimes <- stepTimes - swingTimes # Calculate stance times
-  
+
   # position-based
   stepWidths <- relHeelStrikesData$pos_x # Calculate step width
   stepWidths <- ifelse(relHeelStrikesData$foot == "Left", stepWidths * -1, stepWidths) # Adjust sign based on which foot is stepping
-  #heelStrikeHeights <- 
   stepLengths <- relHeelStrikesData$actual_pos_z # Calculate step lengths
-  
   speed <- stepLengths / stepTimes # Calculate speed
-  
+
   # We ignore the steps onto and away from a target
-  # and also add to the ignore list the outliers in the stepTimes and stepLengths (these are very obvious ways to remove the misdetected steps)
   targetSteps <- heelStrikesData$target
   targetSteps <- targetSteps | lead(heelStrikesData$target, default = FALSE) # add step before
   targetSteps <- targetSteps | lag(heelStrikesData$target, default = FALSE) | lag(heelStrikesData$target, 2, default = FALSE) | lag(heelStrikesData$target, 3, default = FALSE) # add 3 steps after (until foot is placed back onto new position)
   heelStrikesData$targetIgnoreSteps <- targetSteps
-  
-  
+
   # Detect outliers
-  # First, we throw out some incorrect steps that are just physically impossible (for example the first/last step sometimes is filled in as a 0.0s step, which is nonsensical)
-  heelStrikesData$incorrectDetection <-  stepLengths <= 0 | stepTimes < 0.05 | gaitData$toeOffs$pos_y > 1.0 | gaitData$heelStrikes$pos_y > 1.0 | gaitData$toeOffs$pos_y < 0.42 | gaitData$heelStrikes$pos_y < 0.42 | stepLengths > 2 | stepTimes > 2 | stepWidths > 1.0 | stepWidths < -0.5
-  heelStrikesData$incorrectDetection[heelStrikesData$targetIgnoreSteps] <- FALSE
-  
-  # no backwards stepsc | impossibly far step | impossibly fast step | impossibly long step | impossibly wide step | a little bit of negative may be possible, but not more than .5m | impossibly high TO | impossibly high HS | impossibly low TO (below floor) | impossibly low HS (below floor)
-  
+  # First, we throw out some incorrect steps that are just physically impossible
+  heelStrikesData$incorrectDetection <- stepLengths > 2.0 | stepLengths <= 0.0 | stepWidths > 0.5 | stepWidths < -0.0
+  heelStrikesData$incorrectDetection[heelStrikesData$targetIgnoreSteps] <- FALSE # prevent overlap
+
+  # and also add to the ignore list the outliers in the step speed
   alreadyIgnoredSteps <- targetSteps | heelStrikesData$incorrectDetection # we define those because we don't want to use them for calculating the IQRs for the outlier detection
-  IQR_mlp <- 1.5
-  heelStrikesData$outlierSteps <- detect_outliers(stepWidths, alreadyIgnoredSteps, IQR_mlp) | detect_outliers(stepTimes, alreadyIgnoredSteps, IQR_mlp) | detect_outliers(stepLengths, alreadyIgnoredSteps, IQR_mlp) | detect_outliers(gaitData$heelStrikes$pos_y, alreadyIgnoredSteps, 3) | detect_outliers(gaitData$toeOffs$pos_y, alreadyIgnoredSteps, 3)
-  
-  #stepsToIgnore <- outlierSteps
+  IQR_mlp <- 3.0
+  heelStrikesData$outlierSteps <- detect_outliers(speed, alreadyIgnoredSteps, FALSE, IQR_mlp) | detect_outliers(stepTimes, alreadyIgnoredSteps, FALSE, IQR_mlp)# | detect_outliers(stepWidths, alreadyIgnoredSteps, FALSE, IQR_mlp)
+
   # Make a list of all the gait parameters
   gaitParams <- list(
     stepTimes = stepTimes,
-    stanceTimes = stanceTimes,
-    swingTimes = swingTimes,
-    #finalStepWidths = finalStepWidths,
+    # stanceTimes = stanceTimes, ####### These are not working well atm. Toe-offs are not filtered properly
+    # swingStanceRatio = swingTimes / stanceTimes,
+    # swingTimes = swingTimes,
+    # finalStepWidths = finalStepWidths,
     stepLengths = stepLengths,
-    #finalStepLengths = finalStepLengths,
+    # finalStepLengths = finalStepLengths,
     stepWidths = stepWidths,
     speed = speed,
-    heelStrikes = heelStrikesData, # We also add the heelstrikes info as it's interesting for plotting.
+    heelStrikes = heelStrikesData,
     toeOffs = toeOffsData,
     relHeelStrikes = relHeelStrikesData
   )
-  
+
+  gaitParams <- lapply(gaitParams, function(x) {
+    # Check if the element is a vector or data frame
+    if (is.vector(x)) {
+      # Remove the first element for vectors
+      x[-1]
+    } else if (is.data.frame(x)) {
+      # Remove the first row for data frames
+      x[-1, ]
+    } else {
+      x # Return the element unchanged if it is not a vector or data frame
+    }
+  })
+
   return(gaitParams)
 }
 
 
-calculate_target_data <- function(participant, trial){
+calculate_target_data <- function(participant, trial) {
   # get the target tracker results
   targetData <- get_t_data(participant, "steptargets", trial)
   targetData$rel_x <- targetData$foot_x - targetData$target_x
   targetData$rel_z <- targetData$foot_z - targetData$target_z
-  
+
   # calculate total target distance
   targetData$targetDist <- sqrt(targetData$rel_x^2 + targetData$rel_z^2)
-  
+
   return(targetData)
 }
--- runtime/source/pre_processing.R
+++ runtime/source/pre_processing.R
@@ -1,30 +1,30 @@
 #### Some pre-processing we use later in plotting and get_foot_events
 
-adjust_times <- function(dataset, minTime, maxTime=180) { # make sure we start at t=0
+adjust_times <- function(dataset, minTime, maxTime = 180) { # make sure we start at t=0
   dataset$time <- dataset$time - minTime
-  dataset <- subset(dataset, time <= maxTime)#dataset <- subset(dataset)
+  dataset <- subset(dataset, time <= maxTime) # dataset <- subset(dataset)
   return(dataset)
 }
 
-preprocess_data <- function(participant, trialNum){
-  leftFoot = get_t_data(participant, "leftfoot", trialNum)
-  rightFoot = get_t_data(participant, "rightfoot", trialNum)
-  hip = get_t_data(participant, "hip", trialNum)
-  
-  targetData = get_t_data(participant, "steptargets", trialNum)
+preprocess_data <- function(participant, trialNum) {
+  leftFoot <- get_t_data(participant, "leftfoot", trialNum)
+  rightFoot <- get_t_data(participant, "rightfoot", trialNum)
+  hip <- get_t_data(participant, "hip", trialNum)
+
+  targetData <- get_t_data(participant, "steptargets", trialNum)
   leftDisturbance <- get_t_data(participant, "leftdisturbance", trialNum)
   rightDisturbance <- get_t_data(participant, "rightdisturbance", trialNum)
-  #rightFoot <- calc_final_pos(rightFoot, rightDisturbance)
-  #leftFoot <- calc_final_pos(leftFoot, leftDisturbance)
-  minTime <- leftFoot$time[1] #get_p_results(participant,"start_time",trialNum)
-  
-  moveSpeed = get_move_speed(participant)
-  maxTime <- ifelse(get_p_results(participant, "practice",trialNum)=="True",120,180)
-  leftFoot = adjust_times(leftFoot, minTime, maxTime)
-  leftFoot$actual_pos_z = leftFoot$pos_z + moveSpeed * leftFoot$time
-  rightFoot = adjust_times(rightFoot, minTime, maxTime)
-  rightFoot$actual_pos_z = rightFoot$pos_z + moveSpeed * rightFoot$time
-  
+  # rightFoot <- calc_final_pos(rightFoot, rightDisturbance)
+  # leftFoot <- calc_final_pos(leftFoot, leftDisturbance)
+  minTime <- leftFoot$time[1] # get_p_results(participant,"start_time",trialNum)
+
+  moveSpeed <- get_move_speed(participant)
+  maxTime <- ifelse(get_p_results(participant, "practice", trialNum) == "True", 120, 180)
+  leftFoot <- adjust_times(leftFoot, minTime, maxTime)
+  leftFoot$actual_pos_z <- leftFoot$pos_z + moveSpeed * leftFoot$time
+  rightFoot <- adjust_times(rightFoot, minTime, maxTime)
+  rightFoot$actual_pos_z <- rightFoot$pos_z + moveSpeed * rightFoot$time
+
   data <- list(
     leftFoot = leftFoot,
     rightFoot = rightFoot,
@@ -33,15 +33,15 @@ preprocess_data <- function(participant, trialNum){
     hip = adjust_times(hip, minTime, maxTime),
     targetData = adjust_times(targetData, minTime, maxTime)
   )
-  
+
   data <- hardcoded_rotation_fix(data, participant, trialNum)
   return(data)
 }
 
 ######## Added this later as a hardcode fix to get my data rotated properly, because with one of the participants, this was necessary
-hardcoded_rotation_fix <- function(data, participant, trialNum){
-  if (participant == "15" && trialNum %in% c(5,6)) { # saw these trials needed a 4 deg rotation
-    return(rotate_preprocessed_data(data, -4*pi/180))
+hardcoded_rotation_fix <- function(data, participant, trialNum) {
+  if (participant == "15" && trialNum %in% c(5, 6)) { # saw these trials needed a 4 deg rotation
+    return(rotate_preprocessed_data(data, -4 * pi / 180))
   } else {
     return(data)
   }
@@ -58,91 +58,126 @@ rotate_preprocessed_data <- function(data, rotation) {
 # one of the datasets has a wrong rotation in some of the trials, we correct that with this function.
 rotate_y <- function(data, theta) {
   # Create the rotation matrix
-  rotation_matrix <- matrix(c(cos(theta), 0, sin(theta), 
-                              0, 1, 0, 
-                              -sin(theta), 0, cos(theta)), 
-                            nrow = 3, byrow = TRUE)
-  
+  rotation_matrix <- matrix(
+    c(
+      cos(theta), 0, sin(theta),
+      0, 1, 0,
+      -sin(theta), 0, cos(theta)
+    ),
+    nrow = 3, byrow = TRUE
+  )
+
   # Apply the rotation to position columns
   positions <- as.matrix(data[, c("pos_x", "pos_y", "pos_z")])
   rotated_positions <- positions %*% t(rotation_matrix)
-  
+
   # Update the dataframe with rotated positions
   data$pos_x <- rotated_positions[, 1]
   data$pos_y <- rotated_positions[, 2]
   data$pos_z <- rotated_positions[, 3]
-  
+
   # Optionally, if you need to rotate orientation vectors (rot_x, rot_y, rot_z)
   rotations <- as.matrix(data[, c("rot_x", "rot_y", "rot_z")])
   rotated_rotations <- rotations %*% t(rotation_matrix)
-  
+
   data$rot_x <- rotated_rotations[, 1]
   data$rot_y <- rotated_rotations[, 2]
   data$rot_z <- rotated_rotations[, 3]
-  
+
   return(data)
 }
 
-apply_padding_and_filter <- function(column, poly_order, frame_size, fs, cutoff_freq = 5) { 
-  # Detect and remove outliers using a z-score method
-  detect_outliers_filter <- function(column, threshold = 3) {
-    # Calculate the z-scores
-    z_scores <- (column - mean(column, na.rm = TRUE)) / sd(column, na.rm = TRUE)
-    # Identify outliers
-    outliers <- abs(z_scores) > threshold
-    return(outliers)
-  }
-  
-  # Detect outliers
-  outliers <- detect_outliers_filter(column)
-  
-  # Replace outliers with NA
-  column[outliers] <- NA
-  
-  # Interpolate the missing values (linear interpolation)
-  column <- na.approx(column, rule = 2)
-  
-  # Calculate the number of points to pad (half the frame size generally works well)
-  pad_width <- 20
-  
-  # Create mirrored padding
-  padding_start <- rev(column[1:pad_width])
-  padding_end <- rev(column[(length(column) - pad_width + 1):length(column)])
-  
-  # Pad the column
-  padded_column <- c(padding_start, column, padding_end)
-  
-  # Apply Butterworth filter to the padded data
-  b <- butter(poly_order, cutoff_freq / (fs / 2))  # 4th order Butterworth filter
-  filtered_column <- filtfilt(b, padded_column)
-  
-  # Remove the padding
-  filtered_column <- filtered_column[(pad_width + 1):(length(filtered_column) - pad_width)]
-  
-  return(filtered_column)
+# Detect and remove outliers using a z-score method
+detect_outliers_z_scores <- function(data, ignoreSteps = c(FALSE), threshold = 3) {
+  data_filtered <- data[!ignoreSteps] # We don't use the target steps to calculate our mean etc
+  # Calculate the z-scores
+  z_scores <- (data - mean(data_filtered, na.rm = TRUE)) / sd(data_filtered, na.rm = TRUE)
+  # Identify outliers
+  outliers <- abs(z_scores) > threshold
+  return(outliers)
 }
+
+# Detect and remove outliers using a Modified z-score method
+detect_outliers_modified_z_scores <- function(data, ignoreSteps = c(FALSE), threshold = 3.5) {
+  data_filtered <- data[!ignoreSteps] # We don't use the target steps to calculate our med etc
+
+  # Calculate the median of the column
+  med <- median(data_filtered, na.rm = TRUE)
+
+  # Calculate the Median Absolute Deviation (MAD)
+  mad_value <- median(abs(data_filtered - med), na.rm = TRUE)
+
+  # Calculate the Modified z-scores
+  modified_z_scores <- 0.6745 * (data - med) / mad_value
+
+  # Identify outliers based on the threshold
+  outliers <- abs(modified_z_scores) > threshold
+
+  return(outliers)
+}
+
+# detect_outliers <- function(data, ignoreSteps = c(FALSE), IQR_mlp = 1.5) {
+#   data_filtered <- data[!ignoreSteps] # We don't use the target steps to calculate our interquartile ranges.
+
+#   Q1 <- quantile(data_filtered, 0.25, na.rm = TRUE)
+#   Q3 <- quantile(data_filtered, 0.75, na.rm = TRUE)
+#   IQR <- Q3 - Q1
+
+#   # Define the upper and lower bounds for outliers
+#   upper_bound <- Q3 + IQR_mlp * IQR
+#   lower_bound <- Q1 - IQR_mlp * IQR
+
+#   outliers <- !(data >= lower_bound & data <= upper_bound)
+#   return(outliers)
+# }
 
 detect_outliers <- function(data, ignoreSteps, also_mark_ignoreSteps_if_outlier = FALSE, IQR_mlp = 1.5) {
   data_filtered <- data[!ignoreSteps] # We don't use the target steps to calculate our interquartile ranges.
-  
+
   Q1 <- quantile(data_filtered, 0.25)
   Q3 <- quantile(data_filtered, 0.75)
   IQR <- Q3 - Q1
-  
+
   # Define the upper and lower bounds for outliers
   upper_bound <- Q3 + IQR_mlp * IQR
   lower_bound <- Q1 - IQR_mlp * IQR
-  
+
   if (also_mark_ignoreSteps_if_outlier) {
     # Initialize outliers vector as FALSE for all data points
     outliers <- rep(FALSE, length(data))
     # Mark outliers in the data that are not ignored
     outliers[!ignoreSteps] <- !(data[!ignoreSteps] >= lower_bound & data[!ignoreSteps] <= upper_bound)
     return(outliers)
-  }
-  else {
+  } else {
     return(!(data >= lower_bound & data <= upper_bound))
   }
+}
+
+
+apply_padding_and_filter <- function(column, poly_order, fs, cutoff_freq = 5) {
+  # Detect outliers
+  # outliers <- detect_outliers_z_scores(column)
+  # column[outliers] <- NA # Replace outliers with NA
+  # column <- na.approx(column, rule = 2) # Interpolate the missing values (linear interpolation)
+
+  # Calculate the number of points to pad (half the frame size generally works well)
+  pad_width <- 20
+
+  # Create mirrored padding
+  padding_start <- rev(column[1:pad_width])
+  padding_end <- rev(column[(length(column) - pad_width + 1):length(column)])
+
+  # Pad the column
+  padded_column <- c(padding_start, column, padding_end)
+
+  # Apply Butterworth filter to the padded data
+  b <- butter(poly_order, cutoff_freq / (fs / 2)) # 4th order Butterworth filter
+  filtered_column <- filtfilt(b, padded_column)
+
+  # Remove the padding
+  filtered_column <- filtered_column[(pad_width + 1):(length(filtered_column) - pad_width)]
+
+  return(filtered_column)
 }
 
 calculate_step_statistics <- function(data, group_vars = c("participant")) {
@@ -154,31 +189,31 @@ calculate_step_statistics <- function(data, group_vars = c("participant")) {
       included_steps = sum(!heelStrikes.incorrectDetection & !heelStrikes.targetIgnoreSteps & !heelStrikes.outlierSteps),
       removed_steps = sum(heelStrikes.incorrectDetection | heelStrikes.outlierSteps),
       target_steps = sum(heelStrikes.targetIgnoreSteps),
-      .groups = 'drop'
+      .groups = "drop"
     )
-  
+
   # Calculate the average and standard deviation for included steps
   total_steps_avg <- mean(step_summary$total_steps)
   total_steps_sd <- sd(step_summary$total_steps)
-  
+
   # Calculate the average and standard deviation for included steps
   included_steps_avg <- mean(step_summary$included_steps)
   included_steps_sd <- sd(step_summary$included_steps)
-  
+
   # Calculate the average and standard deviation for removed steps
   removed_steps_avg <- mean(step_summary$removed_steps)
   removed_steps_sd <- sd(step_summary$removed_steps)
-  
+
   # Calculate the average and standard deviation for target steps
   target_steps_avg <- mean(step_summary$target_steps)
   target_steps_sd <- sd(step_summary$target_steps)
-  
+
   # Create a result data frame
   result <- data.frame(
     Metric = c("Total Steps", "Included Steps", "Removed Steps", "Target Steps"),
     Average = c(total_steps_avg, included_steps_avg, removed_steps_avg, target_steps_avg),
     SD = c(total_steps_sd, included_steps_sd, removed_steps_sd, target_steps_sd)
   )
-  
+
   return(result)
 }
